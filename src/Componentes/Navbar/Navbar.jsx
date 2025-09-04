fd4fbfcfa821e6858a99b8c1ad142afd434c3724
Kaylin Irina <kaylinhernandez10@gmail.com>
2025-09-04 02:41:56 -0300

    Subida de Nav en react

---
+++ src/Componentes/Navbar/Navbar.jsx
@@ -0,0 +1,31 @@
+import "./Navbar.css"
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <section className='header'>
+        <h1 className= 'logo'>TR<span>UE FOOD</span></h1>
+        <nav className='navbar'>
+            <ul className='nav-links'>
+                <li>
+                    <Link to = "/">Home</Link>
+                </li>
+            </ul>
+        </nav>
+        <div className="icons">
+            <button className='search-button'>
+                <i className='fas fa-search'></i>
+            </button>
+
+            <Link to className='icon-button'>
+              <i className= 'fas fa-shopping-cart'></i>
+              <span className= 'counter'>0</span>
+            </Link>
+        </div>
+        Navbar
+        
+    </section>
+  )
+}
+
+export default Navbar
